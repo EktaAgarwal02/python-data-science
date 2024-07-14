ad5129509293978c4482e4f697ad6b81fd436f4c
EktaAgarwal02 <aekta1819@gmail.com>
2024-07-14 12:24:59 +0530

    # for and while

---
+++ pattern3.py
@@ -0,0 +1,14 @@
+from turtle import *
+speed('fast')
+# pencolor('purple')
+# total = 0
+# while total < 100:
+#     total += 5
+#     fd(total)
+#     lt(90)
+total  = 0
+while total < 1000:
+    total += 5
+    fd(total)
+    lt(60)
+mainloop()
